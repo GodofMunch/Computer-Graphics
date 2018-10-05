5627e62f4462e3530e7bc6d1d0e4a6922791dfa8
David O Sullivan <t00139303@365s.ittralee.ie>
2018-10-05 12:55:17 +0100

    Completed matrix assignment

---
+++ Matrix test/Assets/Matrices.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matrices : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 cameraPosition = new Vector3(10, 1, 48);
+        Vector3 cameraUp = new Vector3(-1, -2, 8);
+        Vector3 cameraLookAt = new Vector3(-2, 8, 2);
+        Vector3 cameraForward = (cameraLookAt - cameraPosition).normalized;
+        cameraUp.Normalize();
+
+        Vector3[] vertices = new Vector3[8];
+        vertices[0] = new Vector3(1, 1, 1);
+        vertices[1] = new Vector3(1, -1, 1);
+        vertices[2] = new Vector3(-1, -1, 1);
+        vertices[3] = new Vector3(-1, 1, 1);
+        vertices[4] = new Vector3(-1, 1, -1);
+        vertices[5] = new Vector3(-1, -1, -1);
+        vertices[6] = new Vector3(1, -1, -1);
+        vertices[7] = new Vector3(1, 1, -1);
+
+        print("ORIGINAL VERTICES + \n");
+        printVector3Array(vertices);
+
+        Vector3 startingAxis = new Vector3(8, -2, -2);
+        startingAxis.Normalize();
+
+        Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.AngleAxis(-45, startingAxis));
+
+        print("ROTATION MATRIX");
+        print(rotationMatrix.ToString());
+
+        Vector3[] verticesAfterRotation = tranformVertices(vertices, rotationMatrix);
+
+        print("VERTICES AFTER ROTATION");
+
+        printVector3Array(verticesAfterRotation);
+
+        Matrix4x4 scalingMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(8, 4, 2));
+
+        print("SCALING MATRIX");
+        print(scalingMatrix.ToString());
+
+        Vector3[] verticesAfterScale = tranformVertices(verticesAfterRotation, scalingMatrix);
+
+        print("VERTICES AFTER SCALING");
+        printVector3Array(verticesAfterScale);
+
+        Matrix4x4 translationMatrix = Matrix4x4.TRS(new Vector3(-2, -4, -1), Quaternion.identity, Vector3.one);
+
+        print("translation matrix");
+        print(translationMatrix.ToString());
+
+        Vector3[] verticesAfterTranslation = tranformVertices(verticesAfterScale, translationMatrix);
+
+        print("VERTICES AFTER TRANSLATION");
+        printVector3Array(verticesAfterTranslation);
+
+        Matrix4x4 singleMatrixofTransformations =   translationMatrix * scalingMatrix * rotationMatrix;
+
+        print("MULTIPLICATION OF MATRICES (T * S * R");
+        print(singleMatrixofTransformations.ToString());
+
+        Vector3[] finalSingleMatrix = tranformVertices(vertices, singleMatrixofTransformations);
+
+        print("POINTS AFTER T*S*R");
+        printVector3Array(finalSingleMatrix);
+
+        Quaternion cameraRotation = Quaternion.LookRotation(cameraForward, cameraUp);
+
+        Matrix4x4 viewingMatrix = Matrix4x4.TRS(-cameraPosition, cameraRotation, Vector3.one);
+
+        print("VIEWING MATRIX");
+        print(viewingMatrix.ToString());
+
+        Vector3[] verticesAfterViewingMatrix = tranformVertices(verticesAfterTranslation, viewingMatrix);
+
+        print("VERTICES AFTER VIEWING MATRIX");
+        printVector3Array(verticesAfterViewingMatrix);
+
+        Matrix4x4 projectionMatrix = Matrix4x4.Perspective(90, 1920 / 1080, 1, 1000);
+
+        print("PROJECTION MATRIX");
+        print(projectionMatrix.ToString());
+
+        Vector3[] verticesAfterProjection = tranformVertices(verticesAfterViewingMatrix, projectionMatrix);
+
+        print("VERTICES AFTER PROJECTION");
+        printVector3Array(verticesAfterProjection);
+
+        Matrix4x4 singleMatrixForEverything = projectionMatrix * viewingMatrix * singleMatrixofTransformations;
+
+        print("SINGLE MATRIX OF EVERYTHING");
+        print(singleMatrixForEverything.ToString());
+
+        Vector3[] finalImage = tranformVertices(vertices, singleMatrixForEverything);
+
+        print("FINAL IMAGE");
+        printVector3Array(finalImage);
+
+        Vector3[] originalVerticesByMatrixForEverything = tranformVertices(vertices, singleMatrixForEverything);
+
+        print("ORIGINAL VERTS BY MATRIX FOR EVERYTHING");
+        printVector3Array(originalVerticesByMatrixForEverything);
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void printVector3Array(Vector3[] arrayToBePrinted)
+    {
+        string array = "";
+        for(int i = 0; i < arrayToBePrinted.Length; i++)
+        {
+            array += arrayToBePrinted[i] + "\n";
+        }
+
+        print(array);
+    }
+
+   
+    public Vector3[] tranformVertices(Vector3[] vertices, Matrix4x4 tranformMatrix)
+    {
+        Vector3[] imageAfterTransformation = new Vector3[8];
+
+        for (int i = 0; i < vertices.Length; i++)
+            imageAfterTransformation[i] = tranformMatrix * new Vector4(vertices[i].x, vertices[i].y, vertices[i].z, 1);
+        
+
+        return imageAfterTransformation;
+    }
+
+
+
+
+}
